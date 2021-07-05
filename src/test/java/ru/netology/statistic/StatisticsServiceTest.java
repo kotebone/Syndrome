bd4d873c00a56a544be09b7b20ca0b1d82af3185
e.fedoseeva <tetstest@list.ru>
2021-07-05 03:44:50 +0300

    add new test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StatisticsServiceTest {
 
- // @Test
+ @Test
   void findMax() {
     StatisticsService service = new StatisticsService();
 
@@ -15,4 +15,16 @@ class StatisticsServiceTest {
 
     assertEquals(expected, actual);
   }
+
+    @Test
+    void findMin() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 3;
+
+        long actual = service.findMin(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
 }
